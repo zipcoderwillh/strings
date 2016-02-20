d4d03f0a29e5412f77b05de3135bc0e4083f865b
willhorton <willhorton@Zips-MBP-4.home>
2016-02-19 20:01:19 -0500

    Initial commit

---
+++ src/main/java/MyStringUtils.java
@@ -0,0 +1,27 @@
+/**
+ * Created by willhorton on 2/19/16.
+ */
+public class MyStringUtils {
+
+    public static String combineArrays(char[] input) {
+
+        String result = "";
+        return result;
+
+    }
+
+    public static String combineArrays(String[] input) {
+
+        String result = "";
+        return result;
+
+    }
+
+    public static String[] splitString(String input) {
+
+        String[] result = new String[5];
+        return result;
+
+    }
+
+}
